4deab05d7aeb16c1739d5fa94af56bd0c8635755
Robert Park <rbpark1@github.com>
2018-08-16 16:35:16 -0700

    added reorganizing fixed delete bug

--- ToDoList/todo.js
+++ ToDoList/todo.js
@@ -1,8 +1,10 @@
+// Get DOM Elements
 let form = document.querySelector("form");
 let list = document.querySelector(".list-group");
 
+// Set local storage
 let items = JSON.parse(localStorage.getItem("items")) || [];
-updateDisplay();
+reloadDisplay();
 
 form.addEventListener("submit", addItem);
 list.addEventListener("click", updateData);
@@ -15,38 +17,54 @@ function addItem(e){
         checked: false
     };
     items.push(newItem);
-    updateDisplay();
-    localStorage.setItem('items', JSON.stringify(items));
+    update();
     this.reset();
-    console.log(items);
 }
 
-function updateDisplay(){
+function reloadDisplay(){
     list.innerHTML = items.map((item, i) => {
         return `
         <li class="list-group-item">
             <div>
                 <input type="checkbox" data-index="${i}" ${item.checked ? 'checked' : ''}/><label>${item.text}</label>
             </div>
-            <button class="btn" data-index="${i}"><i class="fa fa-close"></i></button>
+            <button class="btn" data-index="${i}"><i class="fa fa-close" data-index="${i}"></i></button>
         </li>
         `;
     }).join('');
 }
 
 function updateData(e){
-    console.log(e.target);
-    if(e.target.matches("input")){
+    if(e.target.matches("input")){//checkbox
         let targetIndex = e.target.dataset.index;
         console.log(targetIndex);
         // flip checked
         items[targetIndex].checked = !items[targetIndex].checked;
-        localStorage.setItem('items', JSON.stringify(items));
+
+        organizeItems();
+        update();
     } else if(e.target.matches("button") || e.target.matches("i")){
+        console.log(e.target.parentNode);
         //delete from array
         let targetIndex = e.target.dataset.index;
         items.splice(targetIndex, 1);
-        updateDisplay();
-        localStorage.setItem('items', JSON.stringify(items));
+        update();
     }
+}
+
+// updates data and display
+function update(){
+    reloadDisplay();
+    localStorage.setItem('items', JSON.stringify(items));
+}
+
+// Run this to move checked items to bottom of list and unchecked to top
+function organizeItems(){
+    items.sort((item1, item2) => {
+        if(item1.checked && !item2.checked){
+            return 1;
+        } else{
+            return -1;
+        }
+    })
 }
